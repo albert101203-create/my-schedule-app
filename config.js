066b2bc10613039172492fb95fa58db8c5faabce
Yoochan <yoochan@local>
2026-09-14 01:57:42 +0900

    Connect production Supabase project

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase 프로젝트를 만든 뒤 아래 두 값을 채우세요.
 // 이 값들은 브라우저에 공개되는 값이며, 보안은 supabase/schema.sql의 RLS 정책이 담당합니다.
 window.APP_CONFIG = {
-  supabaseUrl: '',
-  supabasePublishableKey: ''
+  supabaseUrl: 'https://btwrpzgipovzewgairsb.supabase.co',
+  supabasePublishableKey: 'sb_publishable_mcII4MawjMqDsz-NUHR2jg_eoy9e4bx'
 };
